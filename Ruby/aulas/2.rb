04652b6c940034795f017d2b34e889946aa744c5
Emerson Batista <84859510+emersondev21@users.noreply.github.com>
2024-10-01 00:47:01 -0300

    Update 2.rb

--- Ruby/aulas/2.rb
+++ Ruby/aulas/2.rb
@@ -10,30 +10,28 @@
 # Resto: %
 # Exponenciação: **
 
+puts "Insira um número:"
+n1 = gets.chomp.to_i
+puts "Insira outro número:"
+n2 = gets.chomp.to_i
+
+puts "A soma entre #{n1} e #{n2} = #{n1+n2}"
+puts "A subtração entre #{n1} e #{n2} = #{n1-n2}"
+puts "A multiplicação entre #{n1} e #{n2} = #{n1*n2}"
+
+puts "A divisão entre #{n1} e #{n2} = #{n1.to_f/n2.to_f}"
+puts "A rsto da divisão entre #{n1} e #{n2} = #{n1.to_f%n2.to_f}"
+# convertendo o tipo de dado para float com o ".to_f"
+
+puts "A potenciação com base #{n1} e expoente #{n2} = #{n1**n2}"
+
 # Média
-puts "Nota 1:"
+# puts "Nota 1:"
 
 # chomp.to_f => converte o dado para float
-nota1 = gets.chomp.to_f
-puts "Nota 2:"
-nota2 = gets.chomp.to_f
+# nota1 = gets.chomp.to_f
+# puts "Nota 2:"
+# nota2 = gets.chomp.to_f
 
 # Soma os dois valores e divide por 2
-puts "Média = #{(nota1+nota2)/2}"
-
-# Soma
-# 1 + 1   => 2
-# Subtração
-# 2 - 1   => 1
-# Multiplicação
-# 2 * 2   => 4
-# Divisão
-# 10 / 5  => 2
-# 17 / 5    => 3, not 3.4
-# 17 / 5.0  => 3.4
-# Exponenciação
-# 2 ** 2  => 4
-# 3 ** 4  => 81
-# Resto
-# 8 % 2   => 0  (8 / 2 = 4; no remainder)
-# 10 % 4  => 2  (10 / 4 = 2 remainder 2)
+# puts "Média = #{(nota1+nota2)/2}"
