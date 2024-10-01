34d1d31786294a0eb0c065be0fc105a18412ff82
Emerson Batista <84859510+emersondev21@users.noreply.github.com>
2024-10-01 01:33:35 -0300

    Create 3.rb

---
+++ Ruby/aulas/3.rb
@@ -0,0 +1,44 @@
+'''
+Booleanos
+________________________________________________________________
+
+obs.: 
+  T = True (Verdade)
+  F = False (Falso)
+
+AND
+  T + T = T
+  T + F = F
+  F + F = F
+  F + T = F
+
+OR
+  T + T = T
+  T + F = T
+  F + F = F
+  F + T = T
+
+NOT
+  NotTrue = F
+  NotFalse = T
+
+&&
+  T + T = T
+  T + F = F
+  F + F = F
+  F + T = F
+||
+  T + T = T
+  T + F = F
+  F + F = F
+  F + T = F
+!
+  T + T = T
+  T + F = F
+  F + F = F
+  F + T = F
+'''
+
+puts false && true
+puts false || true
+puts !false || true
